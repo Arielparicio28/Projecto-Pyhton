9da300596f176eeaed4f6a9125e801c41886c284
ilaan07 <ilaanserraano0706@gmail.com>
2024-06-11 13:09:49 +0200

    modificaciones doit

--- modificacion-menu.py
+++ modificacion-menu.py
@@ -9,75 +9,73 @@ def modificacion(tabla):
     conexion = conectar()
     cursor = conexion.cursor()
 
-    if tabla == "Clientes": 
-        id_cliente = input("Ingrese el ID del cliente a modificar: ")
+    if tabla == "1": 
+        codigo_cliente = input("Ingrese el codigo de cliente a modificar: ")
         nombre = input("Ingrese el nombre del cliente: ")
         apellido = input("Ingrese el apellido del cliente: ")
-        nif = input("Introduce tu Nif/Nie: ")
+        nif_nie = input("Introduce tu Nif/Nie: ")
         direccion = input("Introduce tu dirección: ")
-        codigoPostal = input("Introduce tu codigo postal: ")
+        cp= input("Introduce tu codigo postal: ")
         provincia = input("Introduce tu provinica: ")
+        poblacion = input ("Introduce tu población: ")
 
-        sql = "UPDATE Clientes SET nombre = %s, apellido = %s, nif_nie = %s, direccion = %s, codigopostal = %s, provincia = %s WHERE id = %s"
-        valores = (nombre, apellido, nif, direccion, codigoPostal, provincia, id_cliente)
+        sql = "UPDATE cliente SET nombre = %s, apellido = %s, nif_nie = %s, direccion = %s, cp = %s, provincia = %s WHERE codigo_cliente = %s"
+        valores = (nombre, apellido, nif_nie, direccion, cp, provincia, codigo_cliente, poblacion)
         cursor.execute(sql, valores)
         conexion.commit()
         print("Cliente modificado exitosamente.")
     
-    elif tabla == "CodigosPostales":
-        id_codigo_postal = input("Ingrese el ID del código postal a modificar: ")
+    elif tabla == "2":
         codigo = input("Escriba un código postal: ")
         descripcion = input("Escriba una descripción: ")
 
 
-        sql = "UPDATE CodigosPostales SET codigo = %s, descripcion = %s WHERE id = %s"
-        valores = (codigo, descripcion, id_codigo_postal)
+        sql = "UPDATE codigopostal SET codigo = %s, descripcion = %s WHERE codigo = %s"
+        valores = (codigo, descripcion)
         cursor.execute(sql, valores)
         conexion.commit()
         print("Código postal modificado exitosamente.")
     
-    elif tabla == "Poblaciones":
-        id_poblacion = input("Ingrese el ID de la población a modificar: ")
-        codigoP = input("Escriba un código postal: ")
-        descripcionP = input("Escriba una descripción: ")
+    elif tabla == "3":
+        codigo = input("Escriba un código postal: ")
+        descripcion = input("Escriba una descripción: ")
 
-        sql = "UPDATE Poblaciones SET codigo = %s, descripcion = %s WHERE id = %s"
-        valores = (codigoP, descripcionP, id_poblacion)
+        sql = "UPDATE Poblaciones SET codigo = %s, descripcion = %s WHERE codigo = %s"
+        valores = (codigo, descripcion)
         cursor.execute(sql, valores)
         conexion.commit()
         print("Población modificada exitosamente.")
     
-    elif tabla == "Provincias":
-        id_provincia = input("Ingrese el ID de la provincia a modificar: ")
-        codigoPro = input("Escriba un código postal: ")
-        descripcionPro = input("Escriba una descripción: ")
+    elif tabla == "4":
+        codigo= input("Escriba un código postal: ")
+        descripcion = input("Escriba una descripción: ")
 
-        sql = "UPDATE Provincias SET codigo = %s, descripcion = %s WHERE id = %s"
-        valores = (codigoPro, descripcionPro, id_provincia)
+        sql = "UPDATE Provincias SET codigo = %s, descripcion = %s WHERE codigo = %s"
+        valores = (codigo, descripcion)
         cursor.execute(sql, valores)
         conexion.commit()
         print("Provincia modificada exitosamente.")
     
-    elif tabla == "Bancos":
-        id_banco = input("Ingrese el ID del banco a modificar: ")
-        nombreBanco = input("Escriba el nombre de su banco: ")
-        iban = input("Escriba el número de cuenta: ")
-        swift = input("Escriba el código internacional: ")
+    elif tabla == "5":
+        codigo_banco = input("Ingrese el ID del banco a modificar: ")
+        nombre_entidad = input("Escriba el nombre de su banco: ")
+        codigo_iban = input("Escriba el número de cuenta: ")
+        codigo_swift = input("Escriba el código internacional: ")
 
-        sql = "UPDATE Bancos SET nombre_entidad = %s, codigo_iban = %s, codigo_swift = %s WHERE id = %s"
-        valores = (nombreBanco, iban, swift, id_banco)
+        sql = "UPDATE Bancos SET nombre_entidad = %s, codigo_iban = %s, codigo_swift = %s WHERE codigo_banco = %s"
+        valores = (nombre_entidad, codigo_iban, codigo_swift, codigo_banco)
         cursor.execute(sql, valores)
         conexion.commit()
-        print("Banco modificado exitosamente.")
+        print("Entidad modificada exitosamente.")
     
-    elif tabla == "DireccionesEnvio":
-        id_direccion_envio = input("Ingrese el ID de la dirección de envío a modificar: ")
-        codigoPosenvio = input("Escriba el código postal de envío: ")
-        poblacionenvio = input("Escriba la población de envío: ")
-        provinciaenvio = input("Escriba la provincia de envío: ")
+    elif tabla == "6":
+        id_envio = input("Ingrese el ID de la dirección de envío a modificar: ")
+        cp = input("Escriba el código postal de envío: ")
+        poblacion = input("Escriba la población de envío: ")
+        provincia = input("Escriba la provincia de envío: ")
 
-        sql = "UPDATE DireccionesEnvio SET codigo_postal = %s, poblacion = %s, provincia = %s WHERE id = %s"
-        valores = (codigoPosenvio, poblacionenvio, provinciaenvio, id_direccion_envio)
+        sql = "UPDATE direccionenvio SET codigo_postal = %s, poblacion = %s, provincia = %s WHERE id_envio = %s"
+        valores = (cp, poblacion, provincia, id_envio)
         cursor.execute(sql, valores)
         conexion.commit()
         print("Dirección de envío modificada exitosamente.")
